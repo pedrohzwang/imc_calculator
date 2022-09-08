24180d8319cef22ff4ecb038a94b7fcf4ad21ae4
Pedro Zwang <pedroh.zwang@gmail.com>
2022-09-07 22:50:04 -0300

    adicionados inputs de texto e inicio das validações

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _infoText = 'Informe seus dados!';
+
+  void _resetFields() {
+    weightController.clear();
+    heightController.clear();
+    setState(() {
+      _infoText = 'Informe seus dados!';
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      String imcString = imc.toStringAsPrecision(4);
+
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso ($imcString)";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso ideal ($imcString)";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente acima do peso ($imcString)";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I ($imcString)";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II ($imcString)";
+      } else if (imc >= 40)  {
+        _infoText = "Obesidade Grau III ($imcString)";
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,31 +53,67 @@ class Home extends StatelessWidget {
         title: const Text('Calculadora de IMC'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _resetFields,
             icon: const Icon(Icons.refresh),
           )
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Icon(
-            Icons.person_outline,
-            size: 120,
-            color: Theme.of(context).colorScheme.primary,
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              contentPadding: const EdgeInsets.all(10),
-              labelText: 'Peso (kg)',
-              labelStyle: TextStyle(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(15, 30, 15, 0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Icon(
+                Icons.person_outline,
+                size: 120,
                 color: Theme.of(context).colorScheme.primary,
               ),
-            ),
-            textAlign: TextAlign.center,
-          )
-        ],
+              TextFormField(
+                controller: weightController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(10),
+                  labelText: 'Peso (kg)',
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              TextFormField(
+                controller: heightController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(10),
+                  labelText: 'Altura (cm)',
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30, bottom: 20),
+                child: SizedBox(
+                  height: 65,
+                  child: ElevatedButton(
+                    onPressed: _calculate,
+                    child: const Text("CALCULAR"),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 65,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: Text(_infoText),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
